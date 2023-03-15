7836dfd453e9ea467b34b2ff6ac9f835c268f466
ZYY3902 <ziningye816@outlook.com>
2023-03-14 22:19:56 -0400

    add index.ts under build

---
+++ build/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express';
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.get('/api/getKey', (req:Request, res:Response) => {
+    const options: {method: 'GET', url: string, header: {'x-functions-key': string}} ={
+        method:'GET',
+        url:'https://warehouse-apps.azurewebsites.net/api/GetShipmentData',
+        header:{
+            'x-functions-key': String(process.env.REACT_APP_API_KEY)
+        }
+    };
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
--- src/globals.ts
+++ src/globals.ts
@@ -1,22 +1,11 @@
-import { useState } from "react"
-
-const [apiKey, setapiKey] = useState<string>("");
 export const API_URL = "https://warehouse-apps.azurewebsites.net/api/GetShipmentData?code=";
 // This is a helper function to generate the headers with the x-functions-key attached
 
-const callBackendAPI = async () => {
-  const response = await fetch('/api/getKey');
-  const body = await response.json();
-  if (response.status !== 200) {
-    throw Error(body.message) 
-  }
-  setapiKey(body)
-};
-export const GET_DEFAULT_HEADERS = () => {
-  var headers = new Headers();
-  // You will need to add another header here
-  // If you do not, the API will reject your request (:
-  headers.append('Content-Type', 'application/json');
-  headers.append('x-functions-key', apiKey);
-  return headers;
-};
+// export const GET_DEFAULT_HEADERS = () => {
+//   var headers = new Headers();
+//   // You will need to add another header here
+//   // If you do not, the API will reject your request (:
+//   headers.append('Content-Type', 'application/json');
+//   headers.append('x-functions-key', apiKey);
+//   return headers;
+// };
