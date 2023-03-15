cb638ede7ba8bccbec43c02b1c2233dae9ea7174
ZYY3902 <ziningye816@outlook.com>
2023-03-15 15:17:00 -0400

    4 try with index.ts

--- build/index.ts
+++ build/index.ts
@@ -4,21 +4,17 @@ import axios from 'axios';
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/api/getKey', async (req: Request, res: Response) => {
-  try {
-    const apiKey = process.env.REACT_APP_API_KEY;
-    const url = 'https://warehouse-apps.azurewebsites.net/api/GetShipmentData';
-    const headers = {
-      'x-functions-key': apiKey
+app.get('/api/getKey', (req:Request, res:Response) => {
+    const options: {method: 'GET', url: string, header: {'x-functions-key': string}} ={
+        method:'GET',
+        url:'https://warehouse-apps.azurewebsites.net/api/GetShipmentData',
+        header:{
+            'x-functions-key': String(process.env.REACT_APP_API_KEY)
+        }
     };
-    const response = await axios.get(url, { headers });
-    const data = response.data;
-    res.send(data);
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
+    axios(options).then(response => console.log(response.data));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`)
+})
